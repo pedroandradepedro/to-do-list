251fd7079ace3b0c51b362ee057251850abeddcf
Pedro Andrade <pedrotrabalhos123@hotmail.com>
2025-07-15 23:25:10 -0300

    Adicionando a function renderTarefas, mapeando o array tarefas e exibindo o que foi digitado na lista dentro da ul em uma li. Após isso adicionando a renderTarefas na adicionarTarefas

--- script.js
+++ script.js
@@ -4,17 +4,34 @@ let buttonElement = document.querySelector("#app button");
 
 let tarefas = []; // craindo array para a lista de tarefas
 
+// adicionando as tarefas na ul>li
+
+function renderTarefas() {
+  listElement.innerHTML = ""; // zerando o que esta na ul
+
+  tarefas.map((texto) => {
+    // .map() -> percorre o que tem no array e pega ele recebendo um parâmetro que é o item digitado
+    let liElement = document.createElement("li");
+    let tarefaTexto = document.createTextNode(texto); // criar texto
+
+    liElement.appendChild(tarefaTexto); // coloar o texto na li
+    listElement.appendChild(liElement); // colocar o li na ul
+  });
+}
+
 // adicionando uma operacao para criar a lista
 
 function adicionarTarefas() {
   if (inputElement.value === "") {
     alert("Digite uma tarefa!");
-    return false; // para a execução da função e não faz o restante dela
+    return false; // parar a execução da função e não fazer o restante dela
   } else {
     let novaTarefa = inputElement.value; // variavel para pegar valor do input
 
     tarefas.push(novaTarefa); // adiciona o que foi digitado no array tarefas
     inputElement.value = ""; // apaga o que foi digitado
+
+    renderTarefas();
   }
 }
 
